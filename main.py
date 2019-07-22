08aed3297c42408f81e8435c79f8a417b6e20ef6
gccurtis <gccurtis001@gmail.com>
2019-07-22 16:58:00 -0700

    Added stylesheets

--- main.py
+++ main.py
@@ -12,10 +12,40 @@ class MainHandler(webapp2.RequestHandler):
         template  = jinja_env.get_template("template/start.html")
         self.response.write(template.render())
 
+class enterHandler(webapp2.RequestHandler):
+    def get(self):
+        template  = jinja_env.get_template("template/enter.html")
+        self.response.write(template.render())
+class selfieHandler(webapp2.RequestHandler):
+    def get(self):
+        template  = jinja_env.get_template("template/selfie.html")
+        self.response.write(template.render())
+class callHandler(webapp2.RequestHandler):
+    def get(self):
+        template  = jinja_env.get_template("template/call.html")
+        self.response.write(template.render())
+class summonHandler(webapp2.RequestHandler):
+    def get(self):
+        template  = jinja_env.get_template("template/summon.html")
+        self.response.write(template.render())
+
+class hideHandler(webapp2.RequestHandler):
+    def get(self):
+        template  = jinja_env.get_template("template/hide.html")
+        self.response.write(template.render())
+class runHandler(webapp2.RequestHandler):
+    def get(self):
+        template  = jinja_env.get_template("template/run.html")
+        self.response.write(template.render())
+
+
 app = webapp2.WSGIApplication([
     ('/',MainHandler),
     ('/',enterHandler),
     ('/selfie',selfieHandler),
-    ('/call',callnHandler),
+    ('/call',callHandler),
     ('/summon',summonHandler),
+    ('/hide',hideHandler),
+    ('/enter',enterHandler),
+    ('/enter',enterHandler),
 ], debug=True)

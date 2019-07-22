cf934f05224b1b6689380ef3ae881fbe66deee38
gccurtis <gccurtis001@gmail.com>
2019-07-22 15:33:45 -0700

    2 test application

---
+++ main.py
@@ -0,0 +1,17 @@
+import webapp2
+import os
+import jinja2
+#import logging
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template  = jinja_env.get_template("template/start.html")
+        self.response.write(template.render())
+
+app = webapp2.WSGIApplication([
+    ('/',MainHandler)
+], debug=True)
